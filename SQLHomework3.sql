f406b6627df158384ddb95eba018178a3dba9447
Mert Acu <153332766+mertacu@users.noreply.github.com>
2024-06-22 16:03:56 +0300

    Add files via upload

---
+++ SQLHomework3.sql
@@ -0,0 +1,7 @@
+--SELECT * FROM country WHERE country LIKE 'A%a';
+
+--SELECT * FROM country WHERE country LIKE '%_____n';
+
+--SELECT title FROM film WHERE title LIKE '%t%t%t%t';
+
+--SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
