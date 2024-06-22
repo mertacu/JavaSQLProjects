05f71af77b7af3b26343880b7d37075e7c1607ad
Mert Acu <153332766+mertacu@users.noreply.github.com>
2024-06-22 16:26:58 +0300

    Add files via upload

---
+++ SQLHomework4.sql
@@ -0,0 +1,9 @@
+--SELECT DISTINCT replacement_cost FROM film;
+
+--SELECT DISTINCT COUNT(replacement_cost) FROM film;
+
+-- DISTINCT COUNT(title) FROM film WHERE title LIKE 'T%' AND rating = 'G';
+
+--SELECT country FROM country WHERE country LIKE '_____';
+
+--SELECT COUNT(city) FROM city WHERE city LIKE 'R&' OR city LIKE '%r';
